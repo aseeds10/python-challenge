8292cc83ac8adc6b2ab08df3cc4cfede6e5143c3
aseeds10 <amorelseeds@gmail.com>
2024-07-15 00:15:30 -0400

    changed file location and saving bc path issues in code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 #import csv using module for it
 import os
 import csv
-bankpath = os.path.join ('..','python-challenge','PyBank', 'Resources', 'budget_data.csv')
+bankpath = os.path.join('..','python-challenge','PyBank','Resources','budget_data.csv')
 #reading using csv moduule. open csv file
 with open(bankpath, encoding='UTF-8') as bankfile:
     bankreader = csv.reader(bankfile)
@@ -15,5 +15,5 @@ with open(bankpath, encoding='UTF-8') as bankfile:
         unique_months.add(month)
 
 # Count the number of unique months
-Num_Months = len(unique_months)
-print("Total Months:",Num_Months)
+NUM_MONTHS = len(unique_months)
+print("Total Months:",NUM_MONTHS)
