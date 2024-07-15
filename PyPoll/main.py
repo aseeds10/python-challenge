b6861f67450ad8b2ff605ab43e97c984de12db29
aseeds10 <amorelseeds@gmail.com>
2024-07-15 14:45:06 -0400

    found unique candidates to use for later

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,7 @@ pollpath = os.path.join('..','Resources','election_data.csv')
 
 #initialize starters
 unique_votes=set()
+unique_candidates=set()
 #add the rest of starters here, once do this step
 
 # Reading using csv module. Open CSV file
@@ -14,11 +15,19 @@ with open(pollpath, encoding="utf-8") as pollfile:
     for row in pollreader:
         ballot_id_column = row[0] #ballot id in 1st column
         vote = ballot_id_column.split(",")[0] #1st element after comma split
-        unique_votes.add(vote) #add unique vote to list after each loop
+        unique_votes.add(vote) #add unique vote to set after each loop
+
+        candidate_column=row[2] #3rd column
+        candidate = candidate_column.split(",")[0] #1st elmen after comma split? 
+        #note to self: I don't think split was necessary but was not sure how to add to set without this
+        unique_candidates.add(candidate) #add unique candidate to set after each loop
 #calculations based on above loops etc
 #calc total votes
 NUM_VOTES=len(unique_votes)
 print("Total Votes: ",NUM_VOTES)
+#calc total candidates. do not need to print
+NUM_CANDIDATES=len(unique_candidates)
+print(NUM_CANDIDATES)
 # Specify the file to write to
 #output_path = os.path.join('..','Analysis','PyPoll_Results.txt')
 # Open the file using "write" mode. Specify the variable to hold the contents
