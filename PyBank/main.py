30f44da0c60fc1fa19d2e5c85992e4c91589a7ad
aseeds10 <amorelseeds@gmail.com>
2024-07-15 02:49:11 -0400

    added code to calculate total at end of period

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,27 @@
-#importing file using modules for it
+# get file and using modules for it
 import os
 import csv
 bankpath = os.path.join('..','Resources','Budget_data.csv')
+
+#initialize starters
+unique_months = set()
+TOTAL_PROFIT_LOSSES = 0
+
 # Reading using csv module. Open CSV file
 with open(bankpath, encoding="utf-8") as bankfile:
     bankreader = csv.reader(bankfile)
-    unique_months = set()
-
-    # Skip the header row
     next(bankreader)  # Skip the header row
     
-    # Loop through the rows in the CSV file
     for row in bankreader:
-        date = row[0]  # Assuming the date is in the first column
-        month = date.split(",")[0]  # Extract the month from the date. 1st element after split
+        date = row[0]  # the date is in the first column
+        month = date.split(",")[0]
         unique_months.add(month)
 
-# Count the number of unique months
+        profit_loss = int(row[1])  # Assuming profit/loss is in the second column
+        TOTAL_PROFIT_LOSSES += profit_loss
+# Calculate total number of months
 NUM_MONTHS = len(unique_months)
-print("Total Months:",NUM_MONTHS)
+print("Total Months:", NUM_MONTHS)
+
+# Calculate net total Profit/Loss
+print("Total: $", TOTAL_PROFIT_LOSSES)
